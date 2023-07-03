742141412e433d978bae5e8bb85e6d0a53b85e48
Paul Thomas <paulthomas2107@gmail.com>
2023-07-03 11:36:55 +0100

    More

--- app/[pokemonName]/page.tsx
+++ app/[pokemonName]/page.tsx
@@ -1,6 +1,7 @@
 import { getPokemon } from '@/lib/pokemonAPI';
 import Image from 'next/image';
 import { PokemonImage } from '@/components/pokemon-image';
+import { Progress } from '@/components/ui/progress';
 
 export default async function PokemonPage({
   params,
@@ -24,7 +25,21 @@ export default async function PokemonPage({
           image={pokemonObject.sprites.other['official-artwork'].front_default}
           name={pokemonName}
         />
-      </div>
+          </div>
+          <h3>Weight: {pokemonObject.weight}</h3>
+          <div className='flex-col'>
+              {pokemonObject.stats.map((statObject: any) => {
+                  const statName = statObject.stat.name;
+                  const statValue = statObject.base_stat;
+
+                  return (
+                      <div className='flex items-stretch' style={{width: "500px"}} key={statName}>
+                          <h3 className='p-3 w-2/4'>{statName}: {statValue}</h3>
+                          <Progress className="w-2/4 m-auto" value={ statValue   } />
+                      </div>
+                  )
+              })}
+          </div>
     </>
   );
 }
