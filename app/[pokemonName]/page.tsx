893ec1128d2ecf55176bb715b9ac0c4e771533de
Paul Thomas <paulthomas2107@gmail.com>
2023-07-03 11:41:07 +0100

    More

--- app/[pokemonName]/page.tsx
+++ app/[pokemonName]/page.tsx
@@ -16,7 +16,10 @@ export default async function PokemonPage({
 
   return (
     <>
-      <h1 className="text-4xl text-bold pt-4">{pokemonName}</h1>
+      <h1 className="text-4xl text-bold pt-4">
+        {' '}
+        {pokemonName.charAt(0).toUpperCase() + pokemonName.slice(1)}{' '}
+      </h1>
       <div
         className="m-4"
         style={{ position: 'relative', width: '300px', height: '300px' }}
@@ -25,21 +28,27 @@ export default async function PokemonPage({
           image={pokemonObject.sprites.other['official-artwork'].front_default}
           name={pokemonName}
         />
-          </div>
-          <h3>Weight: {pokemonObject.weight}</h3>
-          <div className='flex-col'>
-              {pokemonObject.stats.map((statObject: any) => {
-                  const statName = statObject.stat.name;
-                  const statValue = statObject.base_stat;
+      </div>
+      <h3>Weight: {pokemonObject.weight}</h3>
+      <div className="flex-col">
+        {pokemonObject.stats.map((statObject: any) => {
+          const statName = statObject.stat.name;
+          const statValue = statObject.base_stat;
 
-                  return (
-                      <div className='flex items-stretch' style={{width: "500px"}} key={statName}>
-                          <h3 className='p-3 w-2/4'>{statName}: {statValue}</h3>
-                          <Progress className="w-2/4 m-auto" value={ statValue   } />
-                      </div>
-                  )
-              })}
-          </div>
+          return (
+            <div
+              className="flex items-stretch"
+              style={{ width: '500px' }}
+              key={statName}
+            >
+              <h3 className="p-3 w-2/4">
+                {statName}: {statValue}
+              </h3>
+              <Progress className="w-2/4 m-auto" value={statValue} />
+            </div>
+          );
+        })}
+      </div>
     </>
   );
 }
